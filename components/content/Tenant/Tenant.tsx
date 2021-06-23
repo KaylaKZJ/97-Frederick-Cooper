78b099c6eca50c506c451c44d32d7dcd4ff9bf6f
Arno van Staden <arnovanstaden@gmail.com>
2021-06-23 10:06:20 +0200

    remove fade

--- components/content/Tenant/Tenant.tsx
+++ components/content/Tenant/Tenant.tsx
@@ -1,5 +1,4 @@
 import { ITenant } from "../../../utils/types";
-const Fade = require("react-reveal/Fade");
 
 // Components
 import NextImage from "../../UI/Library/NextImage/NextImage";
@@ -11,66 +10,64 @@ const Tenant = (tenant: ITenant) => {
 
 
     return (
-        <Fade >
-            <article className={styles.tenant}>
-                <a href={tenant.websiteURL && tenant.websiteURL}
-                    target="blank"
-                    className={styles.logo}>
-                    <NextImage
-                        src={tenant.logo.asset.url}
-                        width={400}
-                        alt={`${tenant.name} Logo`}
-                        align="Center"
-                        priority
-                    />
-                </a>
-                <div className={styles.content}>
-                    <div className={styles.heading}>
-                        <h2>{tenant.name}</h2>
-                        <h3>{tenant.tagline}</h3>
+        <article className={styles.tenant}>
+            <a href={tenant.websiteURL && tenant.websiteURL}
+                target="blank"
+                className={styles.logo}>
+                <NextImage
+                    src={tenant.logo.asset.url}
+                    width={400}
+                    alt={`${tenant.name} Logo`}
+                    align="Center"
+                    priority
+                />
+            </a>
+            <div className={styles.content}>
+                <div className={styles.heading}>
+                    <h2>{tenant.name}</h2>
+                    <h3>{tenant.tagline}</h3>
+                </div>
+                <div className={styles.info}>
+                    <div className={styles.row}>
+                        <h3>Services:</h3>
+                        <p dangerouslySetInnerHTML={{ __html: tenant.services.join(" <span>|</span> ") }}></p>
                     </div>
-                    <div className={styles.info}>
-                        <div className={styles.row}>
-                            <h3>Services:</h3>
-                            <p dangerouslySetInnerHTML={{ __html: tenant.services.join(" <span>|</span> ") }}></p>
+                    <div className={`${styles.row} ${styles.contact}`}>
+                        <div className={styles.phone}>
+                            <h3>Contact:</h3>
+                            <a href={`tel:${tenant.phone.replace(/ /g, "")}`}>{tenant.phone}</a>
+                            <p>{tenant.contactPerson}</p>
                         </div>
-                        <div className={`${styles.row} ${styles.contact}`}>
-                            <div className={styles.phone}>
-                                <h3>Contact:</h3>
-                                <a href={`tel:${tenant.phone.replace(/ /g, "")}`}>{tenant.phone}</a>
-                                <p>{tenant.contactPerson}</p>
-                            </div>
-                            <div className={styles.links}>
-                                {tenant.websiteURL &&
-                                    <a href={tenant.websiteURL}
-                                        data-tip="Visit Website"
-                                        target="blank">
-                                        <i className="icon-website" />
-                                    </a>}
-                                {tenant.facebookURL &&
-                                    <a href={tenant.facebookURL}
-                                        data-tip="Visit Facebook Page"
-                                        target="blank">
-                                        <i className="icon-facebook" />
-                                    </a>}
-                                {tenant.instagramURL &&
-                                    <a href={tenant.instagramURL}
-                                        data-tip="Visit Instagram Account"
-                                        target="blank">
-                                        <i className="icon-instagram" />
-                                    </a>}
-                                {tenant.twitterURL &&
-                                    <a href={tenant.twitterURL}
-                                        data-tip="Visit Twitter Profile"
-                                        target="blank">
-                                        <i className="icon-twitter" />
-                                    </a>}
-                            </div>
+                        <div className={styles.links}>
+                            {tenant.websiteURL &&
+                                <a href={tenant.websiteURL}
+                                    data-tip="Visit Website"
+                                    target="blank">
+                                    <i className="icon-website" />
+                                </a>}
+                            {tenant.facebookURL &&
+                                <a href={tenant.facebookURL}
+                                    data-tip="Visit Facebook Page"
+                                    target="blank">
+                                    <i className="icon-facebook" />
+                                </a>}
+                            {tenant.instagramURL &&
+                                <a href={tenant.instagramURL}
+                                    data-tip="Visit Instagram Account"
+                                    target="blank">
+                                    <i className="icon-instagram" />
+                                </a>}
+                            {tenant.twitterURL &&
+                                <a href={tenant.twitterURL}
+                                    data-tip="Visit Twitter Profile"
+                                    target="blank">
+                                    <i className="icon-twitter" />
+                                </a>}
                         </div>
                     </div>
                 </div>
-            </article>
-        </Fade>
+            </div>
+        </article>
     )
 }
 
